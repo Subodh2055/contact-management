943a8f8dd4489bb67e20fca2105cf92e7067f382
Subodh Bhandari <subodh.bhandari@sbsolutionsnepal.com>
2022-07-17 11:07:37 +0545

    generating component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContactManagerComponent } from './contact-manager/contact-manager.component';
+import { AddComponentComponent } from './add-component/add-component.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContactManagerComponent,
+    AddComponentComponent,
+    EditContactComponent,
+    ViewContactComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
